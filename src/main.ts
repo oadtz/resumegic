cce20e583a6fada58a3bdcb52605488ad0251aea
Thanapat Pirmphol <t.pirmphol@gmail.com>
2023-08-16 12:22:24 +0700

    no message

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,14 @@
 
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
 import * as express from 'express';
-import * as serverless from 'serverless-http';
-
-const server = express();
+import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
+  return server;
 }
-bootstrap();
 
-module.exports.handler = serverless(server);
+module.exports = bootstrap();
