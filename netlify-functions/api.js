24f9c39a77649c23dfae2f1d28444771df34183c
Thanapat Pirmphol <t.pirmphol@gmail.com>
2023-08-16 13:53:45 +0700

    no message

--- netlify-functions/api.js
+++ netlify-functions/api.js
@@ -1,4 +1,4 @@
-const { handler } = require('../serverless');
+const { handler } = require('../dist/serverless');
 
 exports.handler = async function (event, context) {
   return handler(event, context);
